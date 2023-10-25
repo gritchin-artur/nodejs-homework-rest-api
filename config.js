100098d76d65006a3e3a7ce5d900576764e71253
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-10-25 09:38:27 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const password = "SZXsgVnYIBHD8jhp";
+const DB_HOST = `mongodb+srv://Artur:${password}@cluster0.cocz3n7.mongodb.net/contacts_reader?retryWrites=true&w=majority`;
+
+module.exports = DB_HOST;
