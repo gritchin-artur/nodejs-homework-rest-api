669617c3c178ef2cc3694bc3d093bf7741183ef2
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-10-27 22:20:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-const password = "SZXsgVnYIBHD8jhp";
-const DB_HOST = `mongodb+srv://Artur:${password}@cluster0.cocz3n7.mongodb.net/contacts_reader?retryWrites=true&w=majority`;
 
-module.exports = DB_HOST;
