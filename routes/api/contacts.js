fe072c06b3fe31ee745dd1418666cdf5c3012839
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-10-28 13:25:00 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
@@ -30,5 +31,4 @@ router.patch(
   validateBody(updateFavoriteSchema),
   updateFavorite
 );
-
 module.exports = router;
