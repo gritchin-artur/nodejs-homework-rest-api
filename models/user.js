97161068df6099bf41eec2cba6c96414a5e213ae
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-10-29 14:41:29 +0100

    done

--- models/user.js
+++ models/user.js
@@ -14,12 +14,21 @@ const userSchema = new Schema(
       type: String,
       match: emailRegexp,
       unique: true,
-      required: true,
+      required: [true, "Email is required"],
     },
     password: {
       type: String,
       minlength: 6,
-      required: true,
+      required: [true, "Password is required"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
     },
   },
   { versionKey: false, timestamps: true }
